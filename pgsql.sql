bb640bb0b92709b0493c52dbe5c78ea8e3976246
Pranesh Ulleri <47426277+PraneshUlleri@users.noreply.github.com>
2019-06-21 12:55:36 +0530

    labsheet check point added

---
+++ pgsql.sql
@@ -0,0 +1,13 @@
+﻿insert into instructor values 
+('22222','Einstein','Physics',95000),
+('12121','Wu','Finance',90000),
+('32343','El Said','History',60000),
+('45565','Katz','Physics',75000),
+('95345','Kim','Physics',80000),
+('76766','Crick','Physics',72000),
+('10101','Srinivasan','Comp. Sci.',65000),
+('58583','Califieri','Comp. Sci.',62000),
+('83821','Brandt','Music',92000),
+('15151','Mozart','Physics',40000),
+('33456','Gold','Finance',87000),
+('76543','Singh','Finance',80000);
